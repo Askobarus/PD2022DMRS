02a0321bce353098617cca3b208c70e4f2acc0b1
Askobarus <salnikov.salnikov2010@yandex.ru>
2022-05-26 20:17:48 +0300

    Нужные для работы файлы

--- MatLabNumpy.m
+++ MatLabNumpy.m
@@ -4,15 +4,12 @@ fID = fopen("indreznumpy.txt", 'w'); % Файл со всеми временам
 for i = 1:50
     A = randi([-100 100], 100);
         
-    fileID = fopen("ind"+string(i)+".txt", 'w'); % Файл с матрицой
-    fprintf(fileID, '%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d\n', A);
-    fclose(fileID);
+    fileID = strcat('ind', num2str(i), '.txt');
+    writematrix(A, fileID, 'Delimiter', '\t');
     
     tic
-    fileID = fopen("ind"+string(1)+".txt", 'r'); 
     
-    A1 = fscanf(fileID, '%d\n' ,[100 100]);
-    fclose(fileID);
+    A1 = readmatrix(fileID);
     
     x = unique(A1);
     fid = fopen("numpyrez"+string(i)+".txt", "w"); % Файл со результатом для одной матрицы
