5c55f682315aafccb29d5dece488152234449cf3
Askobarus <salnikov.salnikov2010@yandex.ru>
2022-05-26 20:48:41 +0300

    code and report fixes

--- MatLabSympy.m
+++ MatLabSympy.m
@@ -1,19 +1,20 @@
 clc
 clear;
 syms x
-a = 2
+a = 1
 b = 1
-f = a * x + b
-int(1 / f, x)
+f = 1 / (a * x + b)
+int(f, x)
 
-lim1 = randi([1 100], 1, 100);
-lim2 = randi([1 100], 1, 100);
+lim1 = linspace(1, 100, 100);
+lim2 = linspace(1, 100, 100);
+lim2 = lim2.^2
 fid = fopen("number_2.txt", "w")
 fid2 = fopen("timeNumber2.txt", "w")
 tic
 for i = 1:100
-expr = string(int(1/ f, [lim1(i), lim2(i)])) + newline;
-fwrite(fid, expr);
+    expr =  string(eval(int(f, [lim1(i), lim2(i)]))) + newline;
+    fwrite(fid, expr);
 end
 t = toc
 fwrite(fid2, string(t));
--- Project_sympy_n3.py
+++ Project_sympy_n3.py
@@ -10,8 +10,8 @@ while (a == 0 and b == 0):
     a = int(input("a >> "))
     b = int(input("b >> "))
 
-lim1 = [random.randint(1, 1000) for i in range(100)]
-lim2 = [random.randint(1, 1000) for i in range(100)]
+lim1 = [i for i in range(100)]
+lim2 = [i * i for i in range(100)]
 
 x = sp.Symbol("x")
 
@@ -20,7 +20,7 @@ f = open('number_3.txt', 'w')
 #print(lim2)
 for i in range(100):
     if(a != 0):
-        ans = sp.integrate(1 / (a * x + b), (x, lim1[i], lim2[i]))
+        ans = (sp.integrate(1 / (a * x + b), (x, lim1[i], lim2[i]))).evalf()
     else:
         ans = b * lim2[i] - lim1[i] * b
     f.write(str(ans) + '\n')
