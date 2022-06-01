0be37a331a2b881701241454ce3dda186e9b0f6a
Askobarus <salnikov.salnikov2010@yandex.ru>
2022-06-01 13:48:28 +0300

    ...

--- Project_sympy_n3.py
+++ Project_sympy_n3.py
@@ -10,8 +10,8 @@ while (a == 0 and b == 0):
     a = int(input("a >> "))
     b = int(input("b >> "))
 
-lim1 = [i for i in range(100)]
-lim2 = [i * i for i in range(100)]
+lim1 = [random.randint(1, 1000) for i in range(100)]
+lim2 = [random.randint(1, 1000) for i in range(100)]
 
 x = sp.Symbol("x")
 
@@ -20,9 +20,10 @@ f = open('number_3.txt', 'w')
 #print(lim2)
 for i in range(100):
     if(a != 0):
-        ans = (sp.integrate(1 / (a * x + b), (x, lim1[i], lim2[i]))).evalf()
+        ans = sp.integrate(1 / (a * x + b), (x, lim1[i], lim2[i])).evalf()
     else:
         ans = b * lim2[i] - lim1[i] * b
+        
     f.write(str(ans) + '\n')
 
 f.write(str(time.time() - timing) + " seconds. Programm end")
